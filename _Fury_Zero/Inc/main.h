6c7ef272b21352dc20ffda96c571dfb087534db3
samuelwilliams413 <samuelwilliams413@gmail.com>
2018-04-08 17:32:09 +1000

    Dinner Time

--- _Fury_Zero/Inc/main.h
+++ _Fury_Zero/Inc/main.h
@@ -59,6 +59,10 @@
 
 /* Private define ------------------------------------------------------------*/
 
+#define ADC_A_Pin GPIO_PIN_0
+#define ADC_A_GPIO_Port GPIOA
+#define ADC_B_Pin GPIO_PIN_1
+#define ADC_B_GPIO_Port GPIOA
 #define LD3_Pin GPIO_PIN_3
 #define LD3_GPIO_Port GPIOB
 
