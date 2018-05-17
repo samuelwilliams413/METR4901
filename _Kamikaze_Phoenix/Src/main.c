99ac1944066b22d44a5fbaef5682ab053aaa527c
samuelwilliams413 <samuelwilliams413@gmail.com>
2018-05-17 21:38:12 +1000

    17/05/2018 9:38:12 PM

--- _Kamikaze_Phoenix/Src/main.c
+++ _Kamikaze_Phoenix/Src/main.c
@@ -498,7 +498,7 @@ static void MX_TIM3_Init(void) {
 	htim3.Instance = TIM3;
 	htim3.Init.Prescaler = 0;
 	htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim3.Init.Period = 84000000 / 50000 - 1;
+	htim3.Init.Period = 84000000 / 50 - 1;
 	htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 	if (HAL_TIM_PWM_Init(&htim3) != HAL_OK) {
