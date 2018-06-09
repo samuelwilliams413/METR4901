75c597d15928ce2dec563845118137cfc7fbfc93
samuelwilliams413 <samuelwilliams413@gmail.com>
2018-06-09 18:08:14 +1000

    9/06/2018 6:08:14 PM

--- _Lucky_Boar/Src/circ_buff.c
+++ _Lucky_Boar/Src/circ_buff.c
@@ -10,6 +10,10 @@
 /* External variables --------------------------------------------------------*/
 int out = 0;
 
+/**
+ * @brief	Init the circular buffer
+ * @retval	cbuff : fully initialised circular buffer
+ */
 CBUFF* circ_buff_init(void) {
 	CBUFF* cbuff = (struct CBUFF*) malloc(sizeof(struct CBUFF)*1);
 	cbuff->h = 0;
@@ -19,6 +23,12 @@ CBUFF* circ_buff_init(void) {
 	return cbuff;
 }
 
+/**
+ * @brief	Get a values from the circular buffer and then set that value as zero (removing it from the buffer)
+ * @note
+ * @param	cbuff : circular buffer
+ * @retval	out : value stored at the head of the circular buffer
+ */
 uint8_t get(struct CBUFF* cbuff) {
 	out = cbuff->b[cbuff->h];
 	cbuff->b[cbuff->h] = 0;
@@ -28,12 +38,24 @@ uint8_t get(struct CBUFF* cbuff) {
 	return out;
 }
 
+/**
+ * @brief	Add a value to the circular buffer
+ * @note	This can override values if the buffer is full
+ * @param	cbuff : circular buffer
+ * @param	e : element to be added
+ */
 void put(struct CBUFF* cbuff, uint8_t e) {
 	cbuff->b[cbuff->i] = e;
 	cbuff->i = (((cbuff->i) + 1) % CB_SIZE);
 	return;
 }
 
+/**
+ * @brief	Pours an array of values into the circular buffer
+ * @note	This allows for the putting of multiple values
+ * @param	cbuff : circular buffer
+ * @param	buff : buffer of values to be added to the circular buffer
+ */
 void pour(struct CBUFF* cbuff, uint8_t buff[]) {
 	int i;
 	for (i = 0 ;i < B_SIZE; i++) {
